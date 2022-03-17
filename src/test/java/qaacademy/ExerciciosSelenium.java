139cdd2c963c9a6d6a0174f91350c177ec183666
qaacademy <qaacademy@gmail.com>
2022-03-17 20:21:39 -0300

    Exercicios 04 Listas <li> Selenium

--- src/test/java/qaacademy/ExerciciosSelenium.java
+++ src/test/java/qaacademy/ExerciciosSelenium.java
@@ -2,6 +2,7 @@ package qaacademy;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,17 +11,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ExerciciosSelenium {
     static WebDriver driver;
 
-    @Test
-    public void testesFormularioSelenium() throws InterruptedException {
+    @Before
+    public static void before() throws InterruptedException{
         driver = new ChromeDriver();
         driver.get("http://demo.automationtesting.in/Register.html");
-        Thread.sleep(5000);
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void testesFormularioSelenium() throws InterruptedException {
         driver.findElement(By.xpath("//*[@placeholder='First Name']")).sendKeys("Flavio");
         driver.findElement(By.xpath("//*[@placeholder='Last Name']")).sendKeys("Dias");
         driver.findElement(By.xpath("//*[@id='basicBootstrapForm']/div[2]/div/textarea")).sendKeys("Av Paulista, 509");
         driver.findElement(By.xpath("//*[@ng-model='EmailAdress']")).sendKeys("contato@qaacademy.com.br");
         driver.findElement(By.xpath("//*[@ng-model='Phone']")).sendKeys("11977413512");
-
         // selecionando o radiobutton
         driver.findElement(By.xpath("//*[@ng-model='radiovalue' and @value='Male']")).click();
         // validação
@@ -30,13 +34,20 @@ public class ExerciciosSelenium {
         driver.findElement(By.xpath("//input[@value='Movies']")).click();
         Assert.assertTrue("Checkbox não selecionada",
                 driver.findElement(By.xpath("//input[@value='Movies']")).isSelected());
-
         // Selecionar uma lista
         driver.findElement(By.xpath("//div[@id='msdd']")).click();
-        Thread.sleep(1000);
-        driver.findElement(By.xpath("//a[contains(text(),'Ukrainian')]")).click();
-
-
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[contains(text(),'Portuguese')]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'English')]")).click();
+        boolean linguagemPortuguesSelecionada = driver
+        .findElement(
+                By.xpath("//div[contains(text(),'Portuguese') and @class='ui-autocomplete-multiselect-item']"))
+        .isDisplayed(); 
+        boolean linguagemEnglishSelecionada =driver
+        .findElement(
+                By.xpath("//div[contains(text(),'English') and @class='ui-autocomplete-multiselect-item']"))
+        .isDisplayed();
+        Assert.assertTrue(linguagemPortuguesSelecionada && linguagemEnglishSelecionada);
     }
 
     @AfterClass
